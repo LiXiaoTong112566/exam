e024b5ff0fc8f9cee50fb45e6651d1f2ba17db78
lxt <lxt112566@163.com>
2019-07-17 14:36:17 +0800

    修改了用户管理的文件名

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -15,7 +15,7 @@ app.model(require('./models/questionClass.module').default);
 
 app.model(require('./models/userManage.module').default);
 
-app.model(require("./models/addUser/userData.module").default)
+app.model(require("./models/addUser/userAllData.module").default)
 app.model(require('./models/GradeManage.module').default);
 app.model(require('./models/ClassManage.module').default);
 app.model(require('./models/ExamList.module').default);
